96938705cb5a5e183ed1ff0d65af31590fa04938
AbhinavSrivastava <abhinav.srivastava@caglobal.com>
2024-01-16 18:47:25 +0530

    VCNEw

--- DemoGitTesting/ViewController.swift
+++ DemoGitTesting/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("hi guys")
         // Do any additional setup after loading the view.
     }
 
